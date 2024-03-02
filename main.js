cd9102fb36c2085f17f3f478f0884b49930961be
DUA-UMER <duam95623@gmail.com>
2024-03-03 02:06:14 +0500

    second assaignment

---
+++ main.js
@@ -0,0 +1,15 @@
+var uname = "malik rimsha";
+var ulocation = "karachi";
+var marks = 500;
+var percentage = 80;
+console.log("name:", uname);
+console.log("location:", ulocation);
+console.log("marks", marks);
+console.log("percentage", percentage);
+var str = "1";
+var str2 = str;
+console.log(typeof (str2));
+var num = 2;
+console.log("valus of num", " ", "my lucky number", " ", +num);
+num = 7;
+console.log(num);
